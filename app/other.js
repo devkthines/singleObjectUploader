a898c9943e72bd83978cdfdc7fd48f03c67a478e
Korey Hines <KHines@paqinc.com>
2024-02-28 14:46:09 -0800

    first commit

---
+++ app/other.js
@@ -0,0 +1,55 @@
+
+    // {/* {objects.map((o) => (
+    //       <div key={o.Key}>
+    //         <h6>{o.Key}</h6>
+    //         {/* <Button variant="primary" onClick={() => downloadObject(o.Key)}>Click to Download</Button> */}
+    //       </div>
+    //     ))}
+  // fetchBuckets();
+ //  const fetchBuckets = async () => {
+
+  //     const command = new ListBucketsCommand({});
+
+  //     try {
+  //       const response = await client.send(command);
+  //       setBuckets(response);
+  //       setIsLoading(false); // Set loading to false once buckets are fetched
+  //       console.log("Buckets:", response); // Console log the buckets array
+  //     } catch (err) {
+  //       console.error('this is the error: ',err);
+  //       setIsLoading(false); // Set loading to false in case of error
+  //     }
+  //   };
+      {/* <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.js</code>
+        </p>{" "}
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={100}
+            height={40}
+            priority
+          />
+        </div>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>/*/}
